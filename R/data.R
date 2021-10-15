9360c180cef664aefd8b15ee78c69747d2a6d2fd
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-10-15 16:33:04 +0200

    init

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' gordon atlas
+#'
+#' These parcels had highly homogenous RSFC patterns, 
+#' indicating that they contained one unique RSFC signal; 
+#' furthermore, the parcels were much more homogenous than a 
+#' null model matched for parcel size when tested in two separate datasets. 
+#' Several alternative parcellation schemes were tested this way, and no 
+#' other parcellation was as homogenous as or had as large a difference compared 
+#' with its null model.
+#' annot files were aquired from (\href{https://github.com/faskowit/multiAtlasTT}{faskowit/multiAtlasTT})
+#'
+#' @docType data
+#' @name gordon
+#' @keywords datasets
+#' @family ggseg_atlases ggseg3d_atlases
+#' @references Gordon, E. M., Laumann, T. O., Adeyemo, B., Huckins, J. F., 
+#'    Kelley, W. M., & Petersen, S. E. (2014). Generation and evaluation of
+#'    a cortical area parcellation from resting-state correlations. 
+#'    Cerebral cortex, 26(1), 288-303. Chicago
+#'    (\href{https://pubmed.ncbi.nlm.nih.gov/25316338/}{PubMed})
+#'     
+#' \itemize{
+#'  \item{gordon}{ - gordon atlas}
+#'  \item{gordon_3d}{ - gordon 3d mesh atlas}
+#'}
+#'
+#' @import ggseg
+#' @import ggseg3d
+#' @rdname gordon
+#' @examples
+#' data(gordon)
+#' data(gordon_3d)
+"gordon"
+
+#' @rdname gordon
+"gordon_3d"
+
